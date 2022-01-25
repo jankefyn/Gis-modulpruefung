149eabecdf3479b87d5eb2093db42f7a2ca9848e
jankefyn <72445405+jankefyn@users.noreply.github.com>
2022-01-25 19:35:31 +0100

    noch ein versuch

--- modulpruefung/ersterserver.js
+++ modulpruefung/ersterserver.js
@@ -64,7 +64,6 @@ var P_3_1Server;
         let data = await products.find().toArray();
         if (data.length > 0) {
             let dataString = "";
-            let istKategorieVorhanden = false;
             for (let counter = 0; counter < data.length - 1; counter++) {
                 if (data[counter].name != undefined) {
                     let gefriergutZähler = counter + 1;
@@ -73,37 +72,29 @@ var P_3_1Server;
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🥩") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🧀") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🍅") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🥤") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
                     }
                 }
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🥩") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🧀") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🍅") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🥤") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
             }
             if (_kategorie == "All") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
--- modulpruefung/ersterserver.ts
+++ modulpruefung/ersterserver.ts
@@ -98,7 +98,6 @@ export namespace P_3_1Server {
         let data: Antwort[] = await products.find().toArray();
         if (data.length > 0) {
             let dataString: string = "";
-            let istKategorieVorhanden: boolean = false;
             for (let counter: number = 0; counter < data.length - 1; counter++) {
                 if (data[counter].name != undefined) {
                     let gefriergutZähler: number = counter + 1;
@@ -107,37 +106,37 @@ export namespace P_3_1Server {
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🥩") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
+                       
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🧀") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
+                        
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🍅") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
+                        
                     }
                     if (_kategorie == "Fruits" && data[counter].kategorie == "🥤") {
                         dataString = dataString + " Das Produkt " + gefriergutZähler + ": " + data[counter].name + " " + data[counter].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[counter].ablaufdatum + ",";
-                        istKategorieVorhanden = true;
+                        
                     }
                 }
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🥩") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
+                
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🧀") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
+                
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🍅") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
+                
             }
             if (_kategorie == "Fruits" && data[data.length - 1].kategorie == "🥤") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
-                istKategorieVorhanden = true;
+                
             }
             if (_kategorie == "All") {
                 dataString = dataString + " Das Produkt " + data.length + ": " + data[data.length - 1].name + " " + data[data.length - 1].kategorie + " , ist im Kühlschrank und läuft ab am: " + data[data.length - 1].ablaufdatum;
