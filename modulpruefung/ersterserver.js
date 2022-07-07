bcdb92ae680173ab322d864df496dd4c2ce9ab32
jankefyn <72445405+jankefyn@users.noreply.github.com>
2022-07-07 20:53:21 +0200

    versuch

--- modulpruefung/ersterserver.js
+++ modulpruefung/ersterserver.js
@@ -56,7 +56,7 @@ var TextAdventure;
         if (data.length > 0) {
             let dataString = "";
             for (let counter = 0; counter < 4; counter++) {
-                if (data[counter].name != undefined) {
+                if (data.length >= counter) {
                     dataString = dataString + "Adventure " + counter + 1 + " " + data[counter].name;
                 }
             }
