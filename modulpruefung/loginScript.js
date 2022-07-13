fc6e4ae8477060cbc95bb4f37c69ec2b8f228300
jankefyn <72445405+jankefyn@users.noreply.github.com>
2022-07-13 11:48:59 +0200

    url string für localhost

--- modulpruefung/loginScript.js
+++ modulpruefung/loginScript.js
@@ -7,6 +7,7 @@ var TextAdventure;
     async function submit(_parameter) {
         let formData = new FormData(document.forms[0]);
         let url = "https://softwaredesign-abgabe.herokuapp.com/";
+        //let url: string = "http://localhost:8100/";
         let query = new URLSearchParams(formData);
         if (_parameter == "login") {
             url = url + "/login";
